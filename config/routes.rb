7da0772e0d039bc19069f543c1ea1c1b0010d53c
Yin Yué <yyue87@gmail.com>
2019-06-11 22:24:46 +0200

    add some random in routes & lead page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-  get 'pages/contact'
+  get 'pages', to: 'pages#about'
+  get 'pages', to: 'pages#contact'
+  get 'leadgen/advertising/asdf/lead', to: 'pages#contact', as: 'lead'
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
