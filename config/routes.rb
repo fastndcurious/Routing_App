246b37e3e1ad491323cc933eba8a6ed74925eb4d
Yin Yué <yyue87@gmail.com>
2019-06-12 11:51:52 +0200

    Create an admin namespace with dir reorganized

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'dashboard/main'
-  get 'dashboard/user'
-  get 'dashboard/blog'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin
+    get 'dashboard/main'
+    get 'dashboard/user'
+    get 'dashboard/blog'
+  end
+
   get 'about', to: 'pages#about'
   get 'about', to: 'pages#contact'
   get 'leadgen/advertising/asdf/lead', to: 'pages#contact', as: 'lead'
   resources :blogs
   root to: 'pages#home'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
