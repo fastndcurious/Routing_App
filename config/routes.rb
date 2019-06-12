93e78a7a47507bdb29b21cdaaeeb57fb3e3f3d41
Yin Yué <yyue87@gmail.com>
2019-06-12 11:42:05 +0200

    rails g controller Dashboard main user blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'dashboard/main'
+  get 'dashboard/user'
+  get 'dashboard/blog'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
   get 'about', to: 'pages#contact'
