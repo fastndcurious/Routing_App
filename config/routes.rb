7973e1dbe9f212903c35deb24a07e4c50f0e8b34
Yin Yué <yyue87@gmail.com>
2019-06-12 12:00:29 +0200

    create a post resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  namespace :admin
+
+  namespace :admin do
     get 'dashboard/main'
     get 'dashboard/user'
     get 'dashboard/blog'
   end
 
   get 'about', to: 'pages#about'
-  get 'about', to: 'pages#contact'
+
   get 'leadgen/advertising/asdf/lead', to: 'pages#contact', as: 'lead'
+
   resources :blogs
+  resources :posts
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
